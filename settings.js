1eaa3a7e8235d086051b6fdac3ecc796f0abb4be
xAimzzz <interneths187@gmail.com>
2024-08-10 15:20:11 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -20,18 +20,18 @@ global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
 global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
+global.ownernumber = '4917661566718' //ur owner number
+global.ownername = 'interneths' //ur owner name
 global.websitex = "https://youtu.be/mu5m6aB6P5k"
 global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
 global.themeemoji = '🪀'
 global.wm = "Xeon Bot Inc."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD11' //script link
 global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
+global.author = "interneths\n\n4917661566718"
 global.creator = "916909137213@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.premium = ["4917661566718"] // Premium User
 global.hituet = 0
 
 //bot sett
